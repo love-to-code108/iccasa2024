950f5c29981616a04c6c1f7862bdc820c5dc524d
love.to.code108 <love.to.code108@gmail.com>
2024-08-05 15:53:21 +0530

    i am cleaning out the css too now i will push this for the build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,22 @@
 // IMAGES
-import uemLogoBackground from "../assets/PNG/uemBackgroundLogo.svg"
-import uemLogo from "../assets/PNG/uemLogo2-removebg-preview.png"
-import icassaLogo from "../assets/PNG/ICASSALogo.png"
-import locationIcon from "../assets/PNG/location_on.png"
-import downArrow from "../assets/PNG/downArrow.png"
+// import uemLogoBackground from "../assets/PNG/uemBackgroundLogo.svg"
+// import uemLogo from "../assets/PNG/uemLogo2-removebg-preview.png"
+// import icassaLogo from "../assets/PNG/ICASSALogo.png"
+// import locationIcon from "../assets/PNG/location_on.png"
+// import downArrow from "../assets/PNG/downArrow.png"
 
 
 // CAROUSEL
-import React from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
+// import React from 'react'
+// import useEmblaCarousel from 'embla-carousel-react'
+// import Autoplay from 'embla-carousel-autoplay'
 
 
 export const Home = () => {
 
 
-  // CAROUSEL LOGIC
-  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 2000 })])
-
-
-
-
-
-
-
-
-
-
-
-
 
   return (
-    
+    <div>Hello world</div>
   )
 }
