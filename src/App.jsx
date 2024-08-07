fa4c10532dac2e5c1c0dd57d6e16d3c90cd384b1
love.to.code108 <love.to.code108@gmail.com>
2024-08-07 18:54:34 +0530

    working on the contact us section still not completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import uemLogoBackground from "./assets/PNG/uemBackgroundLogo.svg"
 import uemLogo from "./assets/PNG/uemLogo2-removebg-preview.png"
 import icassaLogo from "./assets/PNG/ICASSALogo.png"
+import locationIcon from "./assets/PNG/location_on.png"
+import { FaFacebookSquare } from "react-icons/fa";
+import facebook from "./assets/PNG/facebook.png"
+import linkedin from "./assets/PNG/linkedin.png"
+import twitter from "./assets/PNG/twitter.png"
+
 
 
 function Banner() {
@@ -103,14 +109,86 @@ function Navbar() {
 function ContactUs() {
 
   return (
-    <div className=" border-2">
-      
+    <div className="contactUs_section 
+    xl:px-32 xl:text-xl ">
 
-      {/*  */}
+
+      {/* THE FIRST BLOCK CONTAINING THE HEADING PHONE NUMBER EMAIL AND UNIVERSITY LOCATION */}
+      <div className=" flex flex-col h-[63%] mr-[15rem]">
+
+
+        {/* CONTACT US HEADING */}
+        <h1 className=" text-5xl font-bold xl:mb-4">Contact Us</h1>
+
+
+        {/* PHONE NUMBER AND EMAIL */}
+        <p>Phone No : +91 123456789</p>
+        <p className=" xl:mb-2">email : email@uem.edu.in</p>
+
+
+
+
+        {/* UEM JAIPUR LOCATION */}
+        <a href="https://www.google.com/maps/place/University+of+Engineering+%26+Management+(UEM),+Jaipur/@27.2124649,75.6976676,17z/data=!3m1!4b1!4m6!3m5!1s0x396cffc30dacc2db:0xb78e5c340c935a95!8m2!3d27.2124649!4d75.7002425!16s%2Fm%2F0hhqh8d!5m1!1e2?entry=ttu" target="_blank" className=" flex justify-center items-center relative xl:right-2">
+
+          <img className=" w-[1.5rem]" src={locationIcon} alt="" />
+          <p>University of Engineering and Management Jaipur</p>
+
+        </a>
+
+
+      </div>
+
+
+
+
+
+
+
+
+      {/* PREVIOUS WEBSITE DETAILS */}
+      <div className=" h-[63%] flex flex-col mr-[4rem]">
+
+        <h1 className="font-bold xl:text-3xl mb-4">Previous Websites</h1>
+
+
+        <a href="https://iccasa2023.uem.edu.in/" target="_blank">2023 : Website Link</a>
+        <a href="https://iccasa.uem.edu.in/" target="_blank">2022 : Website Link</a>
+      </div>
+
+
+
+
+
+
+      {/* GALLERY DEAILS */}
+      <div className=" flex flex-col h-[63%] ">
+        <h1 className=" font-bold xl:text-3xl">Gallery</h1>
+        <p>Memories of previous events</p>
+
+      </div>
+
+
+
+
+      {/* FACEBOOK LINKEDIN AND TWITTER */}
+      <div className="flex justify-center items-center h-[63%] w-[5rem]">
+      <FaFacebookSquare />
+        
+      </div>
+
+
+
+
     </div>
   );
 }
 
+
+
+
+// ***************************************************************************
+// THE APP
 function App() {
 
   return (
