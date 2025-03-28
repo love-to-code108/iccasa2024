64c9bb5b93f78f3cfa702e6e3ea15ab0728fcd81
Keshav <bhardwajkeshav5173@gmail.com>
2025-03-28 11:33:33 +0530

    added link in Conference page

--- src/pages/Conferences.jsx
+++ src/pages/Conferences.jsx
@@ -9,19 +9,19 @@ export const  Conferences=()=>{
             <h1 className="w-[88vw] mt-7 text-4xl font-bold">Past Conferences:</h1>
             <ol className="w-[83vw] list-disc  text-lg mt-7">
                 <li className='text-xl font-semi-bold mb-2'>IEMECON 2024: 12th International Conference on Internet of Everything, Microwave, Embedded, Communication & Networks. <a href="https://iemecon.uem.edu.in/ " className="font-bold text-blue-500 underline">https://iemecon.uem.edu.in/</a></li>
-                <li className='text-xl font-semi-bold mb-2'>3rd International Conference on Human-Centric Smart Computing (ICHCSC-2024) https://ichcsc2024.uem.edu.in/  </li>
-                <li className='text-xl font-semi-bold mb-2'>ICIEMSC 2024 International Conference on Intelligent Energy Management in Smart Cities (ICIEMSC – 2024)  https://iciemsc2024.uem.edu.in/ </li>
+                <li className='text-xl font-semi-bold mb-2'>3rd International Conference on Human-Centric Smart Computing (ICHCSC-2024) <a href="https://ichcsc2024.uem.edu.in/   " className="font-bold text-blue-500 underline">https://ichcsc2024.uem.edu.in/  </a>  </li>
+                <li className='text-xl font-semi-bold mb-2'>ICIEMSC 2024 International Conference on Intelligent Energy Management in Smart Cities (ICIEMSC – 2024)  <a href="https://iciemsc2024.uem.edu.in/  " className="font-bold text-blue-500 underline">https://iciemsc2024.uem.edu.in/ </a> </li>
                 <li className='text-xl font-semi-bold mb-2'>NELTA International Conference 2024 </li>
-                <li className='text-xl font-semi-bold mb-2'>International conference on artificial intelligence in management for business and industrial growth (AIMBIG 2024) https://iccicme.uem.edu.in/ </li>
-                <li className='text-xl font-semi-bold mb-2'>International Conference on Cognitive and Cloud Computing (IC3com 2024)   https://ic3com.uem.edu.in/ </li>
-                <li className='text-xl font-semi-bold mb-2'>ICAP 2024: 2nd International Conference on Advancements in Physiotherapy https://icap.uem.edu.in/ </li>
+                <li className='text-xl font-semi-bold mb-2'>International conference on artificial intelligence in management for business and industrial growth (AIMBIG 2024) <a href="https://iccicme.uem.edu.in/  " className="font-bold text-blue-500 underline">https://iccicme.uem.edu.in/ </a> </li>
+                <li className='text-xl font-semi-bold mb-2'>International Conference on Cognitive and Cloud Computing (IC3com 2024)   <a href="https://ic3com.uem.edu.in/  " className="font-bold text-blue-500 underline">https://ic3com.uem.edu.in/ </a> </li>
+                <li className='text-xl font-semi-bold mb-2'>ICAP 2024: 2nd International Conference on Advancements in Physiotherapy <a href="https://icap.uem.edu.in/  " className="font-bold text-blue-500 underline">https://icap.uem.edu.in/ </a> </li>
             </ol>
             <h1 className="w-[88vw] mt-7 text-4xl font-bold">Upcomig Conferences: </h1>
             <ol className="w-[83vw] list-disc  text-lg mt-7">
-                <li className='text-xl font-semi-bold mb-2'>ICAELS 2025 International Conference On Advances in English Language Studies https://icaels.uem.edu.in/</li>
-                <li className='text-xl font-semi-bold mb-2'>IEMECON 2025: The 13th International Conference on Intelligent Embedded, Micro Electronics, Communication and Optical Networks (IEMECON 2025) https://iemecon2025.uem.edu.in/    </li>
-                <li className='text-xl font-semi-bold mb-2'>4th International Conference on Human-Centric Smart Computing (ICHCSC-2024)   </li>
-                <li className='text-xl font-semi-bold '>AIMBIG Oceania 2025 https://aimbig.uem.edu.in/</li>
+                <li className='text-xl font-semi-bold mb-2'>ICAELS 2025 International Conference On Advances in English Language Studies <a href="https://icaels.uem.edu.in/ " className="font-bold text-blue-500 underline">https://icaels.uem.edu.in/</a></li>
+                <li className='text-xl font-semi-bold mb-2'>IEMECON 2025: The 13th International Conference on Intelligent Embedded, Micro Electronics, Communication and Optical Networks (IEMECON 2025) <a href="https://iemecon2025.uem.edu.in/   " className="font-bold text-blue-500 underline">https://iemecon2025.uem.edu.in/  </a>  </li>
+                <li className='text-xl font-semi-bold mb-2'>4th International Conference on Human-Centric Smart Computing (ICHCSC-2024) <a href="https://ichcsc.uem.edu.in/  " className="font-bold text-blue-500 underline">https://ichcsc.uem.edu.in/ </a>  </li>
+                <li className='text-xl font-semi-bold '>AIMBIG Oceania 2025 <a href="https://aimbig.uem.edu.in/ " className="font-bold text-blue-500 underline">https://aimbig.uem.edu.in/</a></li>
             </ol>
         </div>
     )
